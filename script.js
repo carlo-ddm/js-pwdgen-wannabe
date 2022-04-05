239b861392570d29a690b367c71f2a5e1a4ab407
carlo-ddm <carloduminuco90@gmail.com>
2022-04-05 17:04:47 +0200

    correzione concatenazione

--- script.js
+++ script.js
@@ -2,4 +2,12 @@ const firstName = prompt ('Nome');
 const lastName = prompt ('Cognome');
 const color = prompt ('Colore preferito');
 
-document.getElementById("elementoPerStampareMessaggio").innerHTML = firstName + lastName + color
+console.log (firstName);
+console.log (lastName);
+console.log (color);
+
+document.getElementById("elementoPerStampareMessaggio").innerHTML = firstName +" "+ lastName+" "+ color;
+
+/*
+Ricorda: questo tipo di concatenazione è migliore rispetto alla prima. Permette di creare spazi tra i valori delle variabili.
+*/
