c0802f2ab84e87c973135b88573d85d5533c9e7c
carlo-ddm <carloduminuco90@gmail.com>
2022-04-05 16:45:59 +0200

    Esercizio Concluso (eliminazione console.log)

--- script.js
+++ script.js
@@ -2,8 +2,4 @@ const firstName = prompt ('Nome');
 const lastName = prompt ('Cognome');
 const color = prompt ('Colore preferito');
 
-console.log (firstName);
-console.log (lastName);
-console.log (color);
-
 document.getElementById("elementoPerStampareMessaggio").innerHTML = firstName + lastName + color
